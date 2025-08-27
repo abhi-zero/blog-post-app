de583be0898a1495b46a5b1248b09651114e4df0
abhi-zero <abhi.0zero@gmail.com>
2025-08-27 22:55:57 +0530

    chore: add postsslice reducer and enable rtk devtool

--- src/app/store.js
+++ src/app/store.js
@@ -1,13 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
 import { postApi } from "../api/postApi";
+import postsReducer  from "../features/posts/postsSlice"
 
 export const store = configureStore({
     reducer: {
-        [postApi.reducerPath] : postApi.reducer
+        [postApi.reducerPath] : postApi.reducer,
+        posts : postsReducer 
     },
     middleware : (getDefaultMiddleware) => 
-        getDefaultMiddleware().concat(postApi.middleware)
+        getDefaultMiddleware().concat(postApi.middleware),
+    devTools: true
 })
 
 setupListeners(store.dispatch);
