2b5df386c47f9644f2c04b8238c0ec40aaa382c8
abhi-zero <abhi.0zero@gmail.com>
2025-08-28 12:25:15 +0530

    chore: refactor App component to utilize useInfinitePosts hook and improve post rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,41 @@
-import { useState } from 'react'
+
+import { useEffect, useRef } from 'react';
+import { useInfinitePosts } from './hooks/useInfinitePosts'
+import { useSelector } from 'react-redux';
 
 function App() {
-  const [count, setCount] = useState(0)
+ const {loadMore, isLoading, isError, error, isFetching, totalPages, page} = useInfinitePosts();
+ const allPosts = useSelector((state) =>  state.posts.allIds.map((id) => state.posts.byId[id]))
+
+ if(isLoading) return <h1>Loading.....</h1>
+ if(isError) return <h1>{error}</h1>
 
   return (
     <div
-    className='flex items-center-safe justify-center'
+    className='flex flex-col items-center-safe'
     >
-      <h1>Tailwind working</h1>
+      
+      <ul
+      className='w-[400px] flex flex-col gap-5'
+      >
+        {allPosts.map((post)=> (
+          <li key={post.title}>
+            <h1 
+            className='font-bold text-white'
+            >{post.id} {post.title}</h1>
+            <p>{post.body}</p>
+          </li>
+        ))}
+      </ul>
+
+      <button   
+      className='py-[10px] px-[20px] bg-purple-600 text-white rounded-2xl'
+      onClick={loadMore} 
+      disabled={!totalPages > page}
+      >{totalPages > page ? (isFetching ? 'Loading..' :'Load More'): 'No more posts'}
+        
+        </button>
+      
     </div>
   )
 }
