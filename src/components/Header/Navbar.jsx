20f2aef39eb063a7a12bca99086a4c8f7a0f811d
abhi-zero <abhi.0zero@gmail.com>
2025-08-29 21:28:55 +0530

    fix : colors for light and dark theme

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -16,7 +16,7 @@ useEffect(() => {
   return (
     <header className="top-0 right-0 left-0 fixed md:px-[30px] lg:px-[150px] md:py-[10px] w-[100vw]">
       <nav className="rounded-2xl">
-        <div className="flex justify-between items-center bg-[#0A0A0A] dark:bg-[#FFFFFF] px-[20px] sm:px-[50px] py-[20px] md:rounded-2xl rounded-b-2xl text-[#FFFFFF] dark:text-[#0A0A0A] transition-all duration-300 ease-in-out">
+        <div className="flex justify-between items-center bg-[#e4e3e3] dark:bg-[#272829] px-[20px] sm:px-[50px] py-[20px] md:rounded-2xl rounded-b-2xl text-[#0A0A0A] dark:text-[#FFFFFF] transition-all duration-300 ease-in-out">
           <div>
             <h1 className="font-bold text-2xl">InkFlow</h1>
           </div>
@@ -32,11 +32,11 @@ useEffect(() => {
                     to={link.path}
                     className={({ isActive }) =>
                       [
-                        "relative px-2.5 py-1.5 z-10 after:content-[''] after:absolute after:inset-0 text-[#3B82F6] dark:text-[#2563EB]",
-                        "after:top-full hover:after:top-0 after:bg-[#FFFFFF] dark:after:bg-[#0A0A0A] after:rounded-xl",
+                        "relative px-2.5 py-1.5 z-10 after:content-[''] after:absolute after:inset-0 text-purple-600 dark:text-[#2563EB] transition-all duration-300 ease-in-out",
+                        "after:top-full hover:after:top-0 dark:after:bg-[#f5f5f5] after:bg-[#0A0A0A] after:rounded-xl",
                         "after:transition-all after:duration-300 after:ease-in-out after:z-0 ",
                         isActive
-                          ? "bg-[#FFFFFF] dark:bg-[#0A0A0A] dark:text-[#3B82F6] text-[#2563EB] rounded-xl after:opacity-0 "
+                          ? "dark:bg-[#f5f5f5] bg-[#0A0A0A] text-[#3B82F6] dark:text-[#2563EB] rounded-xl after:opacity-0 "
                           : "",
                       ].join(" ")
                     }
