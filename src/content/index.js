7639404309d0852b38dec76b6eafa3dbee5a2981
abhi-zero <abhi.0zero@gmail.com>
2025-08-29 12:02:45 +0530

    chore : file for static content

---
+++ src/content/index.js
@@ -0,0 +1,20 @@
+
+
+export const navLinks = [
+    {
+        name : 'Home',
+        path : '/'
+    },
+    {
+        name : 'Explore',
+        path : '/explore'
+    },
+     {
+        name : 'Write',
+        path : '/write'
+    },
+     {
+        name : 'Profile',
+        path : '/profile'
+    },
+]
