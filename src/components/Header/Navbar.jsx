c3ff88527e6240690903698b03cf4da7a7e2eaf2
abhi-zero <abhi.0zero@gmail.com>
2025-08-29 13:23:29 +0530

    fix: add tailwind classes and fix dark and light backgrounds

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -1,74 +1,82 @@
-import React, { useState } from "react";
-import { NavLink } from "react-router-dom"; 
+import React, { useEffect, useState } from "react";
+import { NavLink } from "react-router-dom";
 import { navLinks } from "../../content";
 import { GiHamburgerMenu } from "react-icons/gi";
 import ThemeToggle from "./ThemeToggle";
+import { useSelector } from "react-redux";
 
 export default function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+   const theme = useSelector(state => state.theme.theme)
+useEffect(() => {
 
-
-  const [menuOpen, setMenuOpen] = useState(false)
+  console.log(theme);
+  
+},[theme])
   return (
     <header className="top-0 right-0 left-0 fixed md:px-[30px] lg:px-[150px] md:py-[10px] w-[100vw]">
       <nav className="rounded-2xl">
-       <div className="flex justify-between items-center md:dark:bg-[#FFFFFF]/[20%] dark:bg-[white] md:backdrop-blur-md px-[20px] sm:px-[50px] py-[20px] md:rounded-2xl rounded-b-2xl text-[black] md:text-[#FFFFFF]">
-         <div>
-          <h1 className="font-bold text-2xl">InkFlow</h1>
-        </div>
-        {/* desktop */}
-        <div className="hidden md:block"> 
-          <ul className="flex gap-10">
-            {navLinks.map((link) => (
-              <li
-                key={link.name}
-                className="relative px-2.5 py-1.5 overflow-hidden"
-              >
-                <NavLink
-                  to={link.path}
-                  className={({ isActive }) =>
-                    [
-                      "relative px-2.5 py-1.5 z-10 after:content-[''] after:absolute after:inset-0 text-[#3B82F6]",
-                      "after:top-full hover:after:top-0 after:bg-[#FFFFFF] after:rounded-xl",
-                      "after:transition-all after:duration-300 after:ease-in-out after:z-0 ",
-                      isActive ? "dark:bg-[#FFFFFF] rounded-xl after:opacity-0" : "",
-                    ].join(" ")
-                  }
+        <div className="flex justify-between items-center bg-[#0A0A0A] dark:bg-[#FFFFFF] px-[20px] sm:px-[50px] py-[20px] md:rounded-2xl rounded-b-2xl text-[#FFFFFF] dark:text-[#0A0A0A] transition-all duration-300 ease-in-out">
+          <div>
+            <h1 className="font-bold text-2xl">InkFlow</h1>
+          </div>
+          {/* desktop */}
+          <div className="hidden md:block">
+            <ul className="flex gap-10">
+              {navLinks.map((link) => (
+                <li
+                  key={link.name}
+                  className="relative px-2.5 py-1.5 overflow-hidden"
                 >
-                  <span className="z-10 relative">{link.name}</span>
+                  <NavLink
+                    to={link.path}
+                    className={({ isActive }) =>
+                      [
+                        "relative px-2.5 py-1.5 z-10 after:content-[''] after:absolute after:inset-0 text-[#3B82F6] dark:text-[#2563EB]",
+                        "after:top-full hover:after:top-0 after:bg-[#FFFFFF] dark:after:bg-[#0A0A0A] after:rounded-xl",
+                        "after:transition-all after:duration-300 after:ease-in-out after:z-0 ",
+                        isActive
+                          ? "bg-[#FFFFFF] dark:bg-[#0A0A0A] dark:text-[#3B82F6] text-[#2563EB] rounded-xl after:opacity-0 "
+                          : "",
+                      ].join(" ")
+                    }
+                  >
+                    <span className="z-10 relative">{link.name}</span>
+                  </NavLink>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="flex gap-5">
+            <div>
+              <ThemeToggle />
+            </div>
+            <div className="md:hidden block">
+              <button
+                className="text-xl"
+                onClick={() => setMenuOpen((prev) => !prev)}
+              >
+                <GiHamburgerMenu />{" "}
+              </button>
+            </div>
+          </div>
+        </div>
+        {/* {mobile} */}
+        <div
+          className={`md:hidden block bg-[#2563EB] mt-2.5 px-[30px] py-[20px] rounded-xl ${
+            menuOpen ? "opacity-100" : "opacity-0"
+          } transition-all duration-300 ease-in-out`}
+        >
+          <ul className="flex flex-col items-center-safe gap-2.5">
+            {navLinks.map((link) => (
+              <li className="" key={link.name}>
+                <NavLink className="text-white" to={link.path}>
+                  {link.name}
                 </NavLink>
               </li>
             ))}
           </ul>
         </div>
-        <div className="flex gap-5">
-          <div>
-            <ThemeToggle />
-        </div>
-        <div className="md:hidden block">
-          <button className="text-xl"
-          onClick={() => setMenuOpen(prev => !prev)}
-          ><GiHamburgerMenu /> </button>
-        </div>
-        </div>
-       </div>
-       {/* {mobile} */}
-       <div className={`md:hidden block bg-[#3B82F6] mt-2.5 px-[30px] py-[20px] rounded-xl ${menuOpen ? 'opacity-100': 'opacity-0'} transition-all duration-300 ease-in-out`}> 
-            <ul className="flex flex-col items-center-safe gap-2.5">
-              {navLinks.map((link)=>(
-                <li 
-                className=""
-                  key={link.name}
-                >
-                 <NavLink 
-                 className='text-white'
-                 to={link.path}
-                 >
-                   {link.name}
-                 </NavLink>
-                </li>
-              ))}
-            </ul>
-       </div>
       </nav>
     </header>
   );
