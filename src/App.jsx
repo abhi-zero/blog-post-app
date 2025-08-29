7b37c0a9c042df69b36035f97aeaa4bb9e1f4304
abhi-zero <abhi.0zero@gmail.com>
2025-08-29 21:41:44 +0530

    chore : add main element

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 
-import Navbar from "./components/Header/Navbar";
+import {Navbar, } from './components/index'
 import { Outlet } from "react-router";
 import Test from "./components/Test";
 
@@ -8,7 +8,9 @@ function App() {
   return(
    <div className="bg-[#FFFFFF] dark:bg-[#0A0A0A] transition-all duration-700 ease-in-out">
     <Navbar />
-    <Outlet />
+    <main className='pt-[150px]'>
+      <Outlet />
+    </main>
    </div>
   )
 }
