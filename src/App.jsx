117c0c75841f0eeae2e71a0f1a16c2f772428e07
abhi-zero <abhi.0zero@gmail.com>
2025-08-29 13:25:18 +0530

    chore: add componet and outlet for router

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,16 @@
 
-import { useEffect, useRef } from 'react';
-import { useInfinitePosts } from './hooks/useInfinitePosts'
-import { useSelector } from 'react-redux';
+import Navbar from "./components/Header/Navbar";
+import { Outlet } from "react-router";
+import Test from "./components/Test";
+
 
 function App() {
- const {loadMore, isLoading, isError, error, isFetching, totalPages, page} = useInfinitePosts();
- const allPosts = useSelector((state) =>  state.posts.allIds.map((id) => state.posts.byId[id]))
-
- if(isLoading) return <h1>Loading.....</h1>
- if(isError) return <h1>{error}</h1>
-
-  return (
-    <div
-    className='flex flex-col items-center-safe'
-    >
-      
-      <ul
-      className='w-[400px] flex flex-col gap-5'
-      >
-        {allPosts.map((post)=> (
-          <li key={post.title}>
-            <h1 
-            className='font-bold text-white'
-            >{post.id} {post.title}</h1>
-            <p>{post.body}</p>
-          </li>
-        ))}
-      </ul>
-
-      <button   
-      className='py-[10px] px-[20px] bg-purple-600 text-white rounded-2xl'
-      onClick={loadMore} 
-      disabled={!totalPages > page}
-      >{totalPages > page ? (isFetching ? 'Loading..' :'Load More'): 'No more posts'}
-        
-        </button>
-      
-    </div>
+  return(
+   <div className="bg-[#FFFFFF] dark:bg-[#0A0A0A] transition-all duration-700 ease-in-out">
+    <Navbar />
+    <Outlet />
+   </div>
   )
 }
 
-export default App
+export default App;
