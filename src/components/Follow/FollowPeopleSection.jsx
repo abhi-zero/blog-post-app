4f14c80ae69ee70ab06e6b6dcd765e5e6d3fd10c
abhi-zero <abhi.0zero@gmail.com>
2025-08-30 18:28:26 +0530

    chore : add scroll on click functionality

--- src/components/Follow/FollowPeopleSection.jsx
+++ src/components/Follow/FollowPeopleSection.jsx
@@ -3,25 +3,37 @@ import { IoIosArrowDropleftCircle } from "react-icons/io";
 import { IoIosArrowDroprightCircle } from "react-icons/io";
 import { useGetFollowingQuery } from '../../api/personApi';
 import PersonImg from './PersonImg';
-import { useEffect } from "react";
+
 
 
 export default function FollowPeopleSection() {
 
   const {data, isLoading, isError,error} = useGetFollowingQuery({results:10, page:1 })
+  const box = document.querySelector('.box')
+  
 
-  useEffect(()=>{
-    console.log(data);
-  },[data])
+  function slideRight(){
+    let width = box.clientWidth;
+    box.scrollLeft = box.scrollLeft - width
+    console.log(width);
+  }
+
+    function slideLeft(){
+    let width = box.clientWidth;
+    box.scrollLeft = box.scrollLeft + width
+    console.log(width);
+  }
   
   return (
     <section>
         <div className='relative flex items-center-safe gap-2'>
-            <div className="top-[40%] left-0 z-10 lg:static absolute translate-y-[-50%] lg:translate-y-0">
-               <button className='text-[#0A0A0A] dark:text-white text-3xl transition-all duration-300 ease-in-out'><IoIosArrowDropleftCircle /></button>
+            <div className="top-[40%] left-[-2%] z-10 lg:static absolute translate-y-[-50%] lg:translate-y-0">
+               <button 
+               onClick={slideRight}
+               className='text-[#0A0A0A] dark:text-white text-3xl transition-all duration-300 ease-in-out cursor-pointer'><IoIosArrowDropleftCircle /></button>
             </div>
             <div>
-              <ul className="flex gap-2.5 px-[5px] md:px-[0px] max-w-[330px] sm:max-w-[450px] md:max-w-[630px] lg:max-w-[710px] overflow-scroll lg:overflow-x-hidden transition-all duration-300 ease-in-out home-flowwingsec">
+              <ul className="my-scrollbar-hidden flex gap-2.5 px-[5px] md:px-[0px] max-w-[330px] sm:max-w-[450px] md:max-w-[630px] lg:max-w-[710px] overflow-auto lg:overflow-x-hidden transition-all duration-300 ease-in-out box">
                 {isLoading && <h1>loading....</h1>}
                 {
                   isError && <h1>{error}</h1>
@@ -37,8 +49,10 @@ export default function FollowPeopleSection() {
                ))}
               </ul>
             </div>
-            <div className="top-[40%] right-0 z-10 lg:static absolute translate-y-[-50%] lg:translate-y-0">
-              <button className='text-[#0A0A0A] dark:text-white text-3xl transition-all duration-300 ease-in-out'><IoIosArrowDroprightCircle /></button>
+            <div className="top-[39%] right-[-2%] z-10 lg:static absolute translate-y-[-50%] lg:translate-y-0">
+              <button className='text-[#0A0A0A] dark:text-white text-3xl transition-all duration-300 ease-in-out cursor-pointer'
+              onClick={slideLeft}
+              ><IoIosArrowDroprightCircle /></button>
             </div>
         </div>
     </section>
