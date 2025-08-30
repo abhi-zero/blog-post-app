22278f71fb99170aebce3233e61ee660daa258c2
abhi-zero <abhi.0zero@gmail.com>
2025-08-30 15:38:05 +0530

    chore : add api base url of random people

--- variables.js
+++ variables.js
@@ -1,5 +1,6 @@
 const variables = {
-    apiUrl : import.meta.env.VITE_API_URL
+    apiUrl : import.meta.env.VITE_API_URL,
+    personApiUrl : import.meta.env.VITE_PERSON_API_URL,
 }
 
 export default variables
