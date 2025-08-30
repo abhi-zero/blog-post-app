523c28bc5a73db1c0b577b161b4f15517fd439c3
abhi-zero <abhi.0zero@gmail.com>
2025-08-30 22:24:28 +0530

    chore: add postcomponet

--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,13 @@
 import Navbar from "./Header/Navbar";
 import Card from "./posts/Card";
 import FollowPeopleSection from "./Follow/FollowPeopleSection";
+import PostComponent from "./posts/PostComponent"; 
+
 
 
 export {
     Navbar,
     Card,
-    FollowPeopleSection
+    FollowPeopleSection,
+    PostComponent
 }
