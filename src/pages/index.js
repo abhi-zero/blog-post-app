8a91e3fa085bd8dab899214c02567c8656618a53
abhi-zero <abhi.0zero@gmail.com>
2025-08-30 22:26:46 +0530

    chore : index file for pages to import from one place

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import Home from "./Home";
+import Explore from "./Explore";
+import Profile from "./Profile";
+import Write from './Write'
+import Post from "./Post";
+
+export {
+    Home,
+    Explore,
+    Profile,
+    Write,
+    Post
+}
