07700945c29d519cc09d9d395bee0de67dc9b935
abhi-zero <abhi.0zero@gmail.com>
2025-08-30 18:29:13 +0530

    fix: fix the position of card for fire fox

--- src/components/Follow/PersonImg.jsx
+++ src/components/Follow/PersonImg.jsx
@@ -8,7 +8,6 @@ export default function PersonImg(
    }
 ) {
 
-  console.log(imgUrl,name);
   
   return (
     <div className='group'>
@@ -16,7 +15,7 @@ export default function PersonImg(
           <img src={imgUrl} alt={name}  className='border-3 dark:border-[#cacaca] border-black rounded-full min-w-[80px] h-[80px]'/>
           <h1 className='mt-1 font-medium text-[#535252] dark:text-[#918f8f] text-sm text-center'>{name.first}</h1>
         </div>
-        <div className='-z-10 group-hover:z-10 absolute opacity-0 group-hover:opacity-100 shadow-2xl transition-all translate-x-[-25%] duration-300 ease-in-out'>
+        <div className='-z-10 group-hover:z-10 absolute opacity-0 group-hover:opacity-100 shadow-2xl transition-all -translate-x-1/3 duration-300 ease-in-out cursor-pointer'>
           <div className='flex items-center-safe gap-3 bg-[#e4e3e3] dark:bg-[#272829] p-[20px] rounded w-[300px]'>
              <img src={imgUrl} alt={name}  className='border-2 dark:border-[#cacaca] border-black rounded-full min-w-[50px] h-[50px]'/>
              <div className='leading-4'>
