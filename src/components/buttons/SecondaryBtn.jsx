f18c6fc6258e52b345997a038d1b58167c0dda9a
abhi-zero <abhi.0zero@gmail.com>
2025-09-02 00:55:44 +0530

    fix : hover state

--- src/components/buttons/SecondaryBtn.jsx
+++ src/components/buttons/SecondaryBtn.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 
 export default function SecondaryBtn({
-    icon,text,onClick,textColorLight,textColorDark
+    icon, text, onClick, textColorLight, textColorDark, noScale
 }) {
   return (
     
     <div>
          <button
       onClick={onClick && onClick}
-      className={`flex items-center-safe gap-1 ${textColorLight ? textColorLight  : 'text-black'} ${textColorDark ? textColorDark : 'dark:text-white'} rounded transition-all duration-800 ease-in-out cursor-pointer hover:scale-120`}
+      className={`flex items-center-safe gap-1 ${textColorLight ? textColorLight  : 'text-black'} ${textColorDark ? textColorDark : 'dark:text-white'} rounded transition-all duration-800 ease-in-out cursor-pointer ${noScale? '' :'hover:scale-120'}`}
     >
       {icon && icon}
       {text && text}
