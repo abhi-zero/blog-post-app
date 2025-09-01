d64d8a622845e9010199c39ea3b88e5b09b88fd8
abhi-zero <abhi.0zero@gmail.com>
2025-09-02 01:03:29 +0530

    chore: add api key for text editor

--- variables.js
+++ variables.js
@@ -1,6 +1,7 @@
 const variables = {
     apiUrl : import.meta.env.VITE_API_URL,
     personApiUrl : import.meta.env.VITE_PERSON_API_URL,
+    editorApiKey : import.meta.env.VITE_EDITOR_API_KEY,
 }
 
 export default variables
