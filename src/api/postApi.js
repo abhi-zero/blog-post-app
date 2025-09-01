402a3ded1cb5e014c2844bba61f70e61bde00ec1
abhi-zero <abhi.0zero@gmail.com>
2025-09-02 00:55:14 +0530

    chore: add other endpoints

--- src/api/postApi.js
+++ src/api/postApi.js
@@ -5,26 +5,51 @@ console.log(variables);
 
 
 export const postApi = createApi({
-    reducerPath : 'postApi',
-    baseQuery : fetchBaseQuery({baseUrl : variables.apiUrl}),
-    endpoints : (builder) => ({
-        getPosts : builder.query({
-            query : ({limit = 10, page = 1}) => `posts?_limit=${limit}&_page=${page}`,
-            transformResponse: (response, meta) =>{
+    reducerPath: 'postApi',
+    baseQuery: fetchBaseQuery({ baseUrl: variables.apiUrl }),
+    tagTypes: ['Post'],
+    endpoints: (builder) => ({
+        getPosts: builder.query({
+            query: ({ limit = 10, page = 1 }) => `posts?_limit=${limit}&_page=${page}`,
+            transformResponse: (response, meta) => {
                 const totalItems = meta.response.headers.get("X-Total-Count");
                 const totalPages = Math.ceil(totalItems / 10);
 
                 return {
-                    posts : response,
-                    totalCount : Number(totalItems),
+                    posts: response,
+                    totalCount: Number(totalItems),
                     totalPages
                 }
             }
         }),
-        getPostById : builder.query({
+        getPostById: builder.query({
             query: (id) => `posts/${id}`,
+        }),
+        createPost: builder.mutation({
+            query: (data) => ({
+                url: `posts`,
+                method: 'POST',
+                body: data
+            }),
+            invalidatesTags: ['Post']
+
+        }),
+        updatePost: builder.mutation({
+            query: ({id, ...data}) => ({
+                url: `posts/${id}`,
+                method: 'PUT',
+                body: data
+            }),
+            invalidatesTags: ['Post']
+        }),
+        deletePost: builder.mutation({
+            query: (id) => ({
+                url: `posts/${id}`,
+                method: 'DELETE'
+            }),
+            invalidatesTags: ['Post']
         })
     })
 })
 
-export const { useGetPostsQuery, useGetPostByIdQuery} = postApi;
+export const { useGetPostsQuery, useGetPostByIdQuery, useCreatePostMutation, useUpdatePostMutation, useDeletePostMutation } = postApi;
