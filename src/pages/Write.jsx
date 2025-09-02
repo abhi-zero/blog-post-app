e1d7ce6ab0a7b5b80d573efac174032bc79db02b
abhi-zero <abhi.0zero@gmail.com>
2025-09-02 19:21:41 +0530

    chore : add componet for write page

--- src/pages/Write.jsx
+++ src/pages/Write.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import { WritePost } from '../components'
+import { useParams } from 'react-router-dom'
+import { useGetPostByIdQuery } from '../api/postApi'
 
 export default function Write() {
+  const {id} = useParams()
+  const {data: post, isLoading} = useGetPostByIdQuery(id, {skip: !id})
   return (
-    <div>Write</div>
+    <div className='min-h-[calc(100vh-150px)]'>
+      <div>
+        <h1 className='font-bold text-black dark:text-white text-3xl text-center transition-all duration-300 ease-in-out'>{id? 'Edit a ' : 'Write a New '}post</h1>
+      </div>
+      <div>
+        {/* <WritePost /> */}
+       {isLoading ? <>Loading...</> :  <WritePost post={post}/>}
+      </div>
+    </div>
   )
 }
