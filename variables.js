35c35a3e6b043aae3c2b469ae77c631e61f2928c
abhi-zero <abhi.0zero@gmail.com>
2025-09-03 11:05:51 +0530

    feat: add env key and url of appwrite

--- variables.js
+++ variables.js
@@ -2,6 +2,8 @@ const variables = {
     apiUrl : import.meta.env.VITE_API_URL,
     personApiUrl : import.meta.env.VITE_PERSON_API_URL,
     editorApiKey : import.meta.env.VITE_EDITOR_API_KEY,
+    dbUrl :  import.meta.env.VITE_DB_ENDPOINT_URL,
+    projectId :  import.meta.env.VITE_DB_PROJECTID_KEY,
 }
 
 export default variables
