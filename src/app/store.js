8f578ef20ec9d34ebeb2249ee95ca4a76f52c696
abhi-zero <abhi.0zero@gmail.com>
2025-09-03 13:12:56 +0530

    feat: add authApi reducer

--- src/app/store.js
+++ src/app/store.js
@@ -2,6 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
 import { postApi } from "../api/postApi";
 import { personApi } from "../api/personApi";
+import {authApi} from '../api/authApi'
 import postsReducer  from "../features/posts/postsSlice"
 import themeReducer from "../features/theme/themeSlice"
 import notificaionReducer  from "../features/notification/notificationSlice";
@@ -10,6 +11,7 @@ import notificaionReducer  from "../features/notification/notificationSlice";
 export const store = configureStore({
     reducer: {
         [postApi.reducerPath] : postApi.reducer,
+        [authApi.reducerPath] : authApi.reducer,
         [personApi.reducerPath] : personApi.reducer,
         posts : postsReducer, 
         theme : themeReducer,
@@ -18,6 +20,7 @@ export const store = configureStore({
     middleware : (getDefaultMiddleware) => 
         getDefaultMiddleware()
                             .concat(postApi.middleware)
+                            .concat(authApi.middleware)
                             .concat(personApi.middleware),
     devTools: true
 })
