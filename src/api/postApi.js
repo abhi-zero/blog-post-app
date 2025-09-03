d6a73cbf328731ca7d84a3a6e6e8230905ede311
abhi-zero <abhi.0zero@gmail.com>
2025-09-03 11:41:22 +0530

    feat : write postApi according to appwritr sdk

--- src/api/postApi.js
+++ src/api/postApi.js
@@ -1,55 +1,106 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import variables from "../../variables";
+import { database } from "../lib/appwriteClient";
 
 console.log(variables);
-
+const DATABASE_ID = variables.databaseId;
+const POSTS_COLLECTION_ID = variables.postsId;
 
 export const postApi = createApi({
     reducerPath: 'postApi',
-    baseQuery: fetchBaseQuery({ baseUrl: variables.apiUrl }),
+    baseQuery: fetchBaseQuery(),
     tagTypes: ['Post'],
     endpoints: (builder) => ({
         getPosts: builder.query({
-            query: ({ limit = 10, page = 1 }) => `posts?_limit=${limit}&_page=${page}`,
-            transformResponse: (response, meta) => {
-                const totalItems = meta.response.headers.get("X-Total-Count");
-                const totalPages = Math.ceil(totalItems / 10);
-
-                return {
-                    posts: response,
-                    totalCount: Number(totalItems),
-                    totalPages
-                }
+           async queryFn({ limit = 10, offset = 0 }) {
+            try {
+                const response = await database.listDocumnets(
+                    DATABASE_ID,
+                    POSTS_COLLECTION_ID,
+                    [
+                        { limit },
+                        { offset }
+                    ]
+                );
+                return {data : response};
+            } catch (error) {
+                return {error};
             }
+           },
+           providesTags : ["Posts"]
         }),
         getPostById: builder.query({
-            query: (id) => `posts/${id}`,
+            async queryFn(id){
+                try {
+                    const response = await database.getDocument(
+                        DATABASE_ID,
+                        POSTS_COLLECTION_ID,
+                        id
+                    );
+                    return {data : response}
+                } catch (error) {
+                    return {error}
+                }
+            }, 
+            providesTags : ['Posts']
         }),
         createPost: builder.mutation({
-            query: (data) => ({
-                url: `posts`,
-                method: 'POST',
-                body: data
-            }),
+            async queryFn(newPost){
+              try {
+                const reponse = await database.createDocument(
+                    DATABASE_ID,
+                    POSTS_COLLECTION_ID,
+                    "unique()",
+                    newPost
+                );
+                return {data : reponse}               
+              } catch (error) {
+                return {error}
+              }
+            },
             invalidatesTags: ['Post']
 
         }),
         updatePost: builder.mutation({
-            query: ({id, ...data}) => ({
-                url: `posts/${id}`,
-                method: 'PUT',
-                body: data
-            }),
+            async queryFn ({id, ...updateData}){
+                try {
+                    const response = await database.updateDocument(
+                        DATABASE_ID,
+                        POSTS_COLLECTION_ID,
+                        id,
+                        updateData
+                    );
+                    return {data: response}
+                    
+                } catch (error) {
+                    return {error}
+                }
+            },
             invalidatesTags: ['Post']
         }),
         deletePost: builder.mutation({
-            query: (id) => ({
-                url: `posts/${id}`,
-                method: 'DELETE'
-            }),
+           async queryFn ({id}){
+            try {
+                const reponse = await database.deleteDocument(
+                    DATABASE_ID,
+                    POSTS_COLLECTION_ID,
+                    id
+                )
+                return  {data: reponse}
+                
+            } catch (error) {
+                return {error}
+            }
+           },
             invalidatesTags: ['Post']
         })
     })
 })
 
-export const { useGetPostsQuery, useGetPostByIdQuery, useCreatePostMutation, useUpdatePostMutation, useDeletePostMutation } = postApi;
+export const {
+  useGetPostsQuery,
+  useGetPostByIdQuery,
+  useCreatePostMutation,
+  useUpdatePostMutation,
+  useDeletePostMutation,
+} = postApi;
