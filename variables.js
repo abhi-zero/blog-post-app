3011d32dabe1e6f9fcae0149d48626f7aad4e4a5
abhi-zero <abhi.0zero@gmail.com>
2025-09-03 11:20:11 +0530

    feat : add more env varibales of databse and post

--- variables.js
+++ variables.js
@@ -2,8 +2,10 @@ const variables = {
     apiUrl : import.meta.env.VITE_API_URL,
     personApiUrl : import.meta.env.VITE_PERSON_API_URL,
     editorApiKey : import.meta.env.VITE_EDITOR_API_KEY,
-    dbUrl :  import.meta.env.VITE_DB_ENDPOINT_URL,
-    projectId :  import.meta.env.VITE_DB_PROJECTID_KEY,
+    dbUrl :  import.meta.env.VITE_APPWRITE_ENDPOINT_URL,
+    projectId :  import.meta.env.VITE_APPWRITE_PROJECTID_KEY,
+    databaseId :  import.meta.env.VITE_APPWRITE_DATABASE,
+    postsId :  import.meta.env.VITE_APPWRITE_POSTS,
 }
 
 export default variables
