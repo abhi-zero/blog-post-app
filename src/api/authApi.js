de4a35e1b5f67698c1be0a8c14f6fad5be213c2a
abhi-zero <abhi.0zero@gmail.com>
2025-09-03 12:54:05 +0530

    feat: create auth

---
+++ src/api/authApi.js
@@ -0,0 +1,73 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { account } from "appwrite";
+
+
+const authApi = createApi({
+    reducerPath: 'authApi',
+    baseQuery: fetchBaseQuery(),
+    tagTypes: ['Auth'],
+    endpoints: (builder) => ({
+        // regiter (email + password)
+        register: builder.mutation({
+            async queryFn({ email, password, name }) {
+                try {
+                    const response = await account.create(
+                        "unique()",
+                        email,
+                        password,
+                        name
+                    );
+                    return { data: response }
+                } catch (error) {
+                    return { error }
+                }
+            }
+        }),
+        //  login create email session
+        login : builder.mutation({
+            async queryFn({email, password}){
+                try {
+                    const response = await account.createEmailPassWordSession(
+                        email,
+                        password
+                    )
+                    return {data: response}
+                } catch (error) {
+                    return {error}
+                }
+            }
+        }),
+        // get currrent user data
+        getCurrentUser : builder.query({
+            async queryFn(){
+                try {
+                    const response = await account.get();
+                    return {data: response}
+                } catch (error) {
+                    return {error}
+                }
+           },
+           providesTags : ['Auth']
+        }),
+        logout : builder.mutation({
+            async queryFn(){
+                try {
+                    await account.deleteSession("current");
+                    return{data: {success : true}}
+                } catch (error) {
+                    return {error}
+                }
+            }
+        })
+
+    })
+})
+
+
+
+export const {
+  useRegisterMutation,
+  useLoginMutation,
+  useGetCurrentUserQuery,
+  useLogoutMutation,
+} = authApi;
