963ec03f332c6e8db918841c73db2219030976dc
abhi-zero <abhi.0zero@gmail.com>
2025-09-05 17:51:11 +0530

    fix: add type  and add classes value hover, padding

--- src/components/buttons/SecondaryBtn.jsx
+++ src/components/buttons/SecondaryBtn.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 
 export default function SecondaryBtn({
-    icon, text, onClick, textColorLight, textColorDark, noScale
+    icon, text, onClick, textColorLight, textColorDark, noScale , padX ,padY, hover, type
 }) {
   return (
     
     <div>
          <button
       onClick={onClick && onClick}
-      className={`flex items-center-safe gap-1 ${textColorLight ? textColorLight  : 'text-black'} ${textColorDark ? textColorDark : 'dark:text-white'} rounded transition-all duration-800 ease-in-out cursor-pointer ${noScale? '' :'hover:scale-120'}`}
+      type={type && type}
+      className={`flex items-center-safe gap-1 ${textColorLight ? textColorLight  : 'text-black'} ${textColorDark ? textColorDark : 'dark:text-white'} rounded transition-all duration-800 ease-in-out cursor-pointer ${noScale? '' :'hover:scale-120'}  ${padX && padX} ${padY && padY} ${hover && hover}`}
     >
       {icon && icon}
       {text && text}
