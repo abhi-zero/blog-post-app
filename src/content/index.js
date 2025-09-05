cf5c3efc44c25499d18e25e3b3dc94351909679e
abhi-zero <abhi.0zero@gmail.com>
2025-09-05 22:14:28 +0530

    fix: change static array in to a function which return links according to the user

--- src/content/index.js
+++ src/content/index.js
@@ -1,20 +1,20 @@
 
 
-export const navLinks = [
-    {
-        name : 'Home',
-        path : '/'
-    },
-    {
-        name : 'Explore',
-        path : '/explore'
-    },
-     {
-        name : 'Write',
-        path : '/write'
-    },
-     {
-        name : 'Profile',
-        path : '/profile'
-    },
-]
+// content/navLinks.js
+export const getNavLinks = (user) => {
+    if (user) {
+        // logged in
+        return [
+            { name: "Home", path: "/" },
+            { name: "Explore", path: "/explore" },
+            { name: "Write", path: "/write" },
+            { name: "Profile", path: "/profile" },
+        ];
+    } else {
+        // guest
+        return [
+            { name: "Explore", path: "/explore" },
+            { name: "Login", path: "/auth" },
+        ];
+    }
+};
