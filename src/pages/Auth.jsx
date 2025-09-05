b38353c2b960b76048c374425b9ab61741e4e642
abhi-zero <abhi.0zero@gmail.com>
2025-09-05 21:00:37 +0530

    feat: create auth page for route

---
+++ src/pages/Auth.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { AuthForm } from '../components'
+
+
+export default function Auth() {
+  return (
+    <div className='mt-[100px] h-[calc(100vh-250px)]' > 
+        <AuthForm />
+    </div>
+  )
+}
