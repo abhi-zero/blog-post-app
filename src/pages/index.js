0c4f0c6e79bcc9332519637605a529b815c8f500
abhi-zero <abhi.0zero@gmail.com>
2025-09-05 21:00:59 +0530

    feat : add auth for import

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,13 @@ import Explore from "./Explore";
 import Profile from "./Profile";
 import Write from './Write'
 import Post from "./Post";
+import Auth from "./Auth";
 
 export {
     Home,
     Explore,
     Profile,
     Write,
-    Post
+    Post,
+    Auth
 }
