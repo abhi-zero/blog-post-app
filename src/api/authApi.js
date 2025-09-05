c048da91c9ef5065cb5482d9529a7a1d313fb968
abhi-zero <abhi.0zero@gmail.com>
2025-09-05 20:58:57 +0530

    feat:  add code for auto login

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -3,64 +3,59 @@ import { account } from '../lib/appwriteClient';
 
 
 export const authApi = createApi({
-    reducerPath: 'authApi',
-    baseQuery: fakeBaseQuery(),
-    tagTypes: ['Auth'],
-    endpoints: (builder) => ({
-        // regiter (email + password)
-        register: builder.mutation({
-            async queryFn({ email, password, name }) {
-                try {
-                    const response = await account.create(
-                        "unique()",
-                        email,
-                        password,
-                        name
-                    );
-                    return { data: response }
-                } catch (error) {
-                    return { error }
-                }
-            }
-        }),
-        //  login create email session
-        login : builder.mutation({
-            async queryFn({email, password}){
-                try {
-                    const response = await account.createEmailPassWordSession(
-                        email,
-                        password
-                    )
-                    return {data: response}
-                } catch (error) {
-                    return {error}
-                }
-            }
-        }),
-        // get currrent user data
-        getCurrentUser : builder.query({
-            async queryFn(){
-                try {
-                    const response = await account.get();
-                    return {data: response}
-                } catch (error) {
-                    return {error}
-                }
-           },
-           providesTags : ['Auth']
-        }),
-        logout : builder.mutation({
-            async queryFn(){
-                try {
-                    await account.deleteSession("current");
-                    return{data: {success : true}}
-                } catch (error) {
-                    return {error}
-                }
-            }
-        })
-
+  reducerPath: 'authApi',
+  baseQuery: fakeBaseQuery(),
+  tagTypes: ['Auth'],
+  endpoints: (builder) => ({
+    // regiter (email + password)
+    register: builder.mutation({
+      async queryFn({ email, password, name }) {
+        try {
+          await account.create('unique()', email, password, name);
+          await account.createEmailSession(email, password);
+          const user = await account.get();
+          return { data: user }
+        } catch (error) {
+          return { error: { message: error.message, code: error.code } };
+        }
+      }
+    }),
+    //  login create email session
+    login: builder.mutation({
+      async queryFn({ email, password }) {
+        try {
+          await account.createEmailSession(email, password);
+          const user = await account.get();
+          return { data: user }
+        } catch (error) {
+          return { error: { message: error.message, code: error.code } };
+        }
+      }
+    }),
+    // get currrent user data
+    getCurrentUser: builder.query({
+      async queryFn() {
+        try {
+          const response = await account.get();
+          return { data: response }
+        } catch (error) {
+          return { error: { message: error.message, code: error.code } };
+        }
+      },
+      providesTags: ['Auth']
+    }),
+    logout: builder.mutation({
+      async queryFn() {
+        try {
+          await account.deleteSession("current");
+          return { data: { success: true } }
+        } catch (error) {
+          return { error: { message: error.message, code: error.code } };
+        }
+      }
     })
+
+  })
 })
 
 
