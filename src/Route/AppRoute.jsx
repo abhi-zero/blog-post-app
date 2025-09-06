f722481130e047a471b66673358279c1337c04c2
abhi-zero <abhi.0zero@gmail.com>
2025-09-06 13:10:58 +0530

    fix: add protect route

--- src/Route/AppRoute.jsx
+++ src/Route/AppRoute.jsx
@@ -1,13 +1,18 @@
 import React from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Navigate } from "react-router-dom";
 import App from "../App";
 import { Home, Explore, Write, Profile, Post, Auth } from "../pages";
 import { useGetCurrentUserQuery } from "../api/authApi";
 
+function ProtectRoute({user, children}){
+  if(!user) return <Navigate to='/auth' replace />
+  return children;
+}
+
 export default function AppRoutes() {
   const { data: user, isLoading } = useGetCurrentUserQuery();
 
-  if (isLoading) return <p>Loading...</p>; // prevent flicker
+  if (isLoading) return <div className="flex justify-center items-center-safe min-h-[100vh]"><p>Loading...</p></div>; // prevent flicker
 
   const router = createBrowserRouter([
     {
@@ -24,15 +29,27 @@ export default function AppRoutes() {
         },
         {
           path: "/write",
-          element: user ? <Write /> : <Auth />, // protect Write route
+          element: (
+            <ProtectRoute user={user}>
+              <Write />
+            </ProtectRoute>
+          ),// protect Write route
         },
         {
           path: "/write/:id",
-          element: user ? <Write /> : <Auth />,
+          element:  (
+            <ProtectRoute user={user}>
+              <Write />
+            </ProtectRoute>
+          ),
         },
         {
           path: "/profile",
-          element: user ? <Profile /> : <Auth />, // protect Profile route
+          element:  (
+            <ProtectRoute user={user}>
+              <Auth />
+            </ProtectRoute>
+          ), // protect Profile route
         },
         {
           path: "/post/:id",
